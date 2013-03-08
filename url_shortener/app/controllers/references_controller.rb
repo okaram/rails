aab363a2267e73e4bded7f69ee874d672bffe330
Orlando Karam <orlando.karam@gmail.com>
2013-03-07 23:45:48 -0500

    users can only access their own references

--- url_shortener/app/controllers/references_controller.rb
+++ url_shortener/app/controllers/references_controller.rb
@@ -1,9 +1,16 @@
+
 class ReferencesController < ApplicationController
 	before_filter :authenticate_user!
+	
+	def verifyAccess
+		if(@reference.user_id!=current_user.id)
+			raise "Invalid access, can only access *your* references"
+		end
+	end
   # GET /references
   # GET /references.json
   def index
-    @references = Reference.all
+    @references = Reference.where(:user_id => current_user.id)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -15,7 +22,7 @@ class ReferencesController < ApplicationController
   # GET /references/1.json
   def show
     @reference = Reference.find(params[:id])
-
+	verifyAccess
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @reference }
@@ -36,6 +43,7 @@ class ReferencesController < ApplicationController
   # GET /references/1/edit
   def edit
     @reference = Reference.find(params[:id])
+    verifyAccess
   end
 
   # POST /references
@@ -59,7 +67,7 @@ class ReferencesController < ApplicationController
   # PUT /references/1.json
   def update
     @reference = Reference.find(params[:id])
-
+	verifyAccess
     respond_to do |format|
       if @reference.update_attributes(params[:reference])
         format.html { redirect_to @reference, notice: 'Reference was successfully updated.' }
@@ -75,6 +83,7 @@ class ReferencesController < ApplicationController
   # DELETE /references/1.json
   def destroy
     @reference = Reference.find(params[:id])
+    verifyAccess
     @reference.destroy
 
     respond_to do |format|
