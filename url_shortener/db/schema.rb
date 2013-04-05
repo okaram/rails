61518c61d5558ba0ac415f7eaf6f55b962991d9c
Orlando Karam <okaram@www.(none)>
2013-04-04 22:30:43 -0400

    fixing bootstrap layout

--- url_shortener/db/schema.rb
+++ url_shortener/db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(:version => 20130308022856) do
   create_table "references", :force => true do |t|
     t.string   "url"
     t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.integer  "visit_count"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "visit_count", :default => 0
   end
 
   create_table "users", :force => true do |t|
